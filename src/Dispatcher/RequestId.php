329c1e487cfd7bb4da285ca6f3a650600bea5136
Thomas Gelf <thomas@gelf.net>
2026-04-02 10:30:38 +0200

    RequestId: just a hint

--- src/Dispatcher/RequestId.php
+++ src/Dispatcher/RequestId.php
@@ -5,5 +5,5 @@ namespace IMEdge\SnmpEngine\Dispatcher;
 final class RequestId
 {
     public const MIN_ID = 1;
-    public const MAX_ID = 2 ** 31 - 1;
+    public const MAX_ID = 2 ** 31 - 1; // Hint: RFC3416 says 214783647, but the errata corrects this
 }
